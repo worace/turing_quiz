0f3d3c5d0ca7a40492304951696cb5f33e46de71
worace <horace.d.williams@gmail.com>
2015-02-11 13:34:23 -0700

    swap answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,50 +1,5 @@
-class AnswersController < ApplicationController
-  include QuestionNested
-  before_action :set_answer, only: [:show, :edit, :update, :destroy]
-
-  # GET /answers/new
-  def new
-    @answer = Answer.new
+class AnswersController < QuestionResourcesController
+  def resource
+    Answer
   end
-
-  # GET /answers/1/edit
-  def edit
-  end
-
-  # POST /answers
-  def create
-    @answer = Answer.new(answer_params.merge(question: @question))
-
-    if @answer.save
-      redirect_to quiz_path(@question.quiz), notice: 'Answer was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /answers/1
-  def update
-    if @answer.update(answer_params)
-      redirect_to quiz_path(@answer.question.quiz), notice: 'Answer was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /answers/1
-  def destroy
-    @answer.destroy
-    redirect_to quiz_path(@answer.question.quiz), notice: 'Answer was successfully destroyed.'
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_answer
-      @answer = Answer.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def answer_params
-      params.require(:answer).permit(:body, :question_id)
-    end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,3 @@
 class Answer < ActiveRecord::Base
-  belongs_to :question
-  validates_presence_of :question_id
+  include QuestionChild
 end
